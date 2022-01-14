c3ae35c28903ed1494faafd30aae07b7edb89c78
Shivani Saini <52560286+shivani3495@users.noreply.github.com>
2022-01-14 12:35:38 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,8 +1,7 @@
 provider "aws"{
     #profile = "Shivani"
     region = "ap-south-1"
-    access_key = "AKIAR6HBMQJH43XKO4IZ"
-    secret_key = "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"
+    
 
 }
    
